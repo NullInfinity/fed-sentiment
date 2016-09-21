deb2f0b98fd3a0b8ec53836b6b007c5c8141843f
Alex Thorne <alex@alexthorne.net>
2016-09-21 20:11:11 +0100

    Initial commit

--- sentiment.R
+++ sentiment.R
@@ -1,6 +1,7 @@
 library('syuzhet')
 library('readr')
 library('ggplot2')
+library('rvest')
 
 sentiment <- function(name) {
   text.data <- read_file(paste0(name, '.txt'))
@@ -9,8 +10,8 @@ sentiment <- function(name) {
   list(name=name, text.lines=text.lines, text.sentiment=syuzhet.vector)
 }
 
-apple.sentiment <- sentiment('apple')
-fed.sentiment <- sentiment('fed')
-
 ggplot(data.frame(x=fed.sentiment$text.sentiment), aes(x=x)) + geom_histogram(binwidth=.25)
 ggplot(data.frame(x=apple.sentiment$text.sentiment), aes(x=x)) + geom_histogram(binwidth=.25)
+
+fomc.minutes.site <- read_html('https://www.federalreserve.gov/monetarypolicy/fomccalendars.htm')
+fomc.links <- fomc.minutes.site %>% html_nodes(".minutes a:nth-child(2)") %>% html_attr('href')
